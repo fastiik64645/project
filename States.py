6e310a8e9574292141d4d0841a0472e1448344e9
Fastiik <fedrpop089@gmail.com>
2025-05-27 16:52:15 +0700

    first

---
+++ States.py
@@ -0,0 +1,38 @@
+from aiogram.fsm.state import StatesGroup, State
+
+
+class Captcha_users(StatesGroup):
+    status = State()
+
+
+class UserStats(StatesGroup):
+    user_id = State()
+
+
+class AddBalanceCasino(StatesGroup):
+    amount = State()
+
+
+class NewKefGame(StatesGroup):
+    value = State()
+
+
+#Текст
+class AdminText(StatesGroup):
+    text = State()
+    send = State()
+
+
+class AdminPhotoText(StatesGroup):
+    text = State()
+    photo = State()
+    send_photo = State()
+
+
+class NewUrlAdmin(StatesGroup):
+    url = State()
+
+
+
+class AcceptRules(StatesGroup):
+    waiting_accept = State()
